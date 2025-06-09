0931719059684beb5786fc1a4d6a4bd56be7eb8d
vicenteas1 <vicenteas.estudios8059@gmail.com>
2025-06-09 01:20:31 -0400

    Se agrega seccion contacto y confian en nosotros. Se agrega responsivo al carrucel

--- Lab3/proy/src/dist/js/carrousel.js
+++ Lab3/proy/src/dist/js/carrousel.js
@@ -1,8 +1,10 @@
 function scrollCarousel(direction) {
-    const carousel = document.getElementById('product-carousel');
-    const scrollAmount = 150; 
-    carousel.scrollBy({
-        left: direction * scrollAmount,
-        behavior: 'smooth'
-    });
+  const carousel = document.getElementById('product-carousel');
+  const card = carousel.querySelector('.flip-card');
+  const cardWidth = card.offsetWidth + 16; // sumamos el gap (~1rem = 16px)
+  const scrollAmount = cardWidth * 1; // 2 tarjetas por click
+  carousel.scrollBy({
+    left: direction * scrollAmount,
+    behavior: 'smooth'
+  });
 }
